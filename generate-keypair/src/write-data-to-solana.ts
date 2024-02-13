6996009a84fdf2d0fffc378bb443197c5d453121
repananiromoney <repana@niro.money>
2024-02-13 14:48:32 +0530

    Added write data to solana file

---
+++ generate-keypair/src/write-data-to-solana.ts
@@ -0,0 +1,46 @@
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+import {
+    Connection,
+    Keypair,
+    SystemProgram,
+    LAMPORTS_PER_SOL,
+    Transaction,
+    sendAndConfirmTransaction,
+    PublicKey,
+    clusterApiUrl,
+} from "@solana/web3.js";
+
+import "dotenv/config"
+
+(async () => {
+    const fromKeypair = getKeypairFromEnvironment("SECRET_KEY");
+    const toKeypair = new PublicKey("Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod");
+
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+    // const airdropSignature = await connection.requestAirdrop(
+    //     fromKeypair.publicKey,
+    //     2 * LAMPORTS_PER_SOL
+    // );
+
+    // const latestBlockHash = await connection.getLatestBlockhash();
+
+    // await connection.confirmTransaction({
+    //     blockhash: latestBlockHash.blockhash,
+    //     lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+    //     signature: airdropSignature,
+    // });
+
+    const lamportsToSend = 1_000_000;
+
+    const transferTransaction = new Transaction().add(
+        SystemProgram.transfer({
+            fromPubkey: fromKeypair.publicKey,
+            toPubkey: toKeypair,
+            lamports: lamportsToSend,
+        })
+    );
+
+    await sendAndConfirmTransaction(connection, transferTransaction, [
+        fromKeypair,
+    ]);
+})();
